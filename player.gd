30c3cc5d076c7bcf3c46a19ee8ece48e1b50d8a3
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-02-22 16:56:10 +0300

    upd

--- player.gd
+++ player.gd
@@ -2,8 +2,9 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 6
+const JUMP_VELOCITY = 8
 
+var leg = 'left'
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,9 +22,16 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if (velocity.x > 0):
+			$colobok_anim_v3/AnimationPlayer.play("Armature_002Action")
+			#$colobok_anim_v3/AnimationPlayer.play('Armature_002Action')
+		elif (velocity.x < 0):
+			#$colobok_anim_v3/AnimationPlayer.play("Armature_001Action")
+			$colobok_anim_v3/AnimationPlayer.play('Armature_001Action')
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		$colobok_anim_v3/AnimationPlayer.stop()
 
 	move_and_slide()
 	
@@ -33,3 +41,13 @@ func _physics_process(delta: float) -> void:
 	#camera_position.z = lerp(camera_position.z, position.z, 0.08)
 	camera_position.y = lerp(camera_position.y, position.y, 0.08)
 	$Camera_Controller.position = camera_position
+	
+	#$colobok_anim2/AnimationPlayer.queue("leftAction")
+	#$colobok_anim2/AnimationPlayer.queue("rightAction")
+	#if (leg == 'left'):
+		#leg = 'right'
+		#$colobok_anim2/AnimationPlayer.queue("leftAction")
+	#else:
+		#leg = 'left'
+		#$colobok_anim2/AnimationPlayer.queue("rightAction")
+	
